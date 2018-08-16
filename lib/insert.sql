4297ef5b5ce6f8daeb802c787ed0efb100ade4f8
Jason Viger <jason.viger@gmail.com>
2018-08-16 12:58:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,60 @@
-INSERT into projects (title, category, funding_goal, start_date,end_date) VALUES ('a','a',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('b','a',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('c','b',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('d','c',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('e','c',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('f','a',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('g','a',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('h','d',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('i','e',10,'10/1/19','12/31/19');
-insert into projects (title, category, funding_goal, start_date,end_date) VALUES ('j','b',10,'10/1/19','12/31/19');
+INSERT into projects(title, category, funding_goal, start_date,end_date) VALUES ('a','a',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('b','a',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('c','b',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('d','c',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('e','c',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('f','a',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('g','a',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('h','d',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('i','e',10,'10/1/19','12/31/19');
+insert into projects(title, category, funding_goal, start_date,end_date) VALUES ('j','b',10,'10/1/19','12/31/19');
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into users (name, age) VALUES ('a',10)
+insert into pledges (amount, user_id, project_id) VALUES (100,10,1)
+insert into pledges (amount, user_id, project_id) VALUES (100,17,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,10)
+insert into pledges (amount, user_id, project_id) VALUES (100,11,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,7)
+insert into pledges (amount, user_id, project_id) VALUES (100,2,8)
+insert into pledges (amount, user_id, project_id) VALUES (100,5,9)
+insert into pledges (amount, user_id, project_id) VALUES (100,7,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,3)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,3)
+insert into pledges (amount, user_id, project_id) VALUES (100,10,1)
+insert into pledges (amount, user_id, project_id) VALUES (100,17,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,10)
+insert into pledges (amount, user_id, project_id) VALUES (100,11,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,7)
+insert into pledges (amount, user_id, project_id) VALUES (100,2,8)
+insert into pledges (amount, user_id, project_id) VALUES (100,5,9)
+insert into pledges (amount, user_id, project_id) VALUES (100,7,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,3)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,3)
+insert into pledges (amount, user_id, project_id) VALUES (100,10,1)
+insert into pledges (amount, user_id, project_id) VALUES (100,17,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,10)
+insert into pledges (amount, user_id, project_id) VALUES (100,11,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,7)
+insert into pledges (amount, user_id, project_id) VALUES (100,2,8)
+insert into pledges (amount, user_id, project_id) VALUES (100,5,9)
+insert into pledges (amount, user_id, project_id) VALUES (100,7,2)
+insert into pledges (amount, user_id, project_id) VALUES (100,1,3)
+insert into pledges (amount, user_id, project_id) VALUES (100,8,3)
